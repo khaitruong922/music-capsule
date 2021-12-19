2450acdcde2ec24716ee54061c5b707b8e41dab0
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2021-12-19 13:11:07 +0700

    First commit

---
+++ src/Home.tsx
@@ -0,0 +1,8 @@
+import { Box } from '@chakra-ui/react'
+import { FC } from 'react'
+
+const Home: FC = () => {
+	return <Box>Home</Box>
+}
+
+export default Home
---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+	fonts: {
+		body: `Montserrat, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, 
+        Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif`,
+	},
+})
+
+export default theme
