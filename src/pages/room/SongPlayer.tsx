e33a06644cb7e77107926c4eeb7088d39813f44b
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2022-04-01 01:09:31 +0700

    Audio sync better

--- src/pages/room/SongPlayer.tsx
+++ src/pages/room/SongPlayer.tsx
@@ -147,8 +147,8 @@ const SongPlayer: FC = () => {
         audio.src = url
         audio.load()
         try {
-            audio.currentTime = startTime ? Date.now() / 1000 - startTime : 0
             await audio.play()
+            audio.currentTime = startTime ? Date.now() / 1000 - startTime : 0
             playingRef.current = true
             setAutoplayBlocked(false)
             // console.log('good')
