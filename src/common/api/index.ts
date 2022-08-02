de18b19ccd30d31d7a37dba50837f0b9c305a401
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2022-08-02 14:39:59 +0700

    default api and socket endpoints

--- src/common/api/index.ts
+++ src/common/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const url = process.env.REACT_APP_API_URL
+const url = process.env.REACT_APP_API_URL || "http://localhost:3001"
 
 const api = axios.create({
     withCredentials: true,
